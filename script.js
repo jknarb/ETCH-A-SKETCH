ba70e9c2e2ed2fc20c0072215c562b81543156ff
Jay Narbonne <krissada.narbonne@gmail.com>
2020-11-03 22:40:00 +0700

    Create 16 divs inside container. Complete

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("noob");
+const div = document.createElement("div");
+const containerDiv1 = document.querySelector("#container1");
+const containerDiv2 = document.querySelector("#container2");
+const containerDiv3 = document.querySelector("#container3");
+
+function createGrid() {
+    for(i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-color");
+        containerDiv1.appendChild(div);
+    }
+
+    for(i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-color");
+        containerDiv2.appendChild(div);
+    }
+
+    for(i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-color");
+        containerDiv3.appendChild(div);
+    }
+};
+
+createGrid();
+
+// To create next:
+// 1. create 16 container divs with id="container"
+// 2. create variable with array of container divs
+// 3. run for each statement to create 16 divs with class of grid-color
+
